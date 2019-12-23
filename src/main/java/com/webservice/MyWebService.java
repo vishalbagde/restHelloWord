79d49d8ff1d2c5290436f3679d4627b8fd5d93a9
vishalbagde <vishalbagde.mmm@gmail.com>
2019-12-23 15:46:47 +0530

    initial commit

---
+++ src/main/java/com/webservice/MyWebService.java
@@ -0,0 +1,21 @@
+package com.webservice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class MyWebService extends Application {
+	
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public MyWebService() {
+		singletons.add(new BookService());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+
+}
